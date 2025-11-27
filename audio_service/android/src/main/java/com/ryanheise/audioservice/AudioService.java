a34c31a5a5dbfdbfb9fd45c064643d8015b30a40
MSOB7YY <mohamedsobhy201566@gmail.com>
2025-11-27 19:42:48 +0200

    fix: avoid nullpo `AudioService.instance` in `AudioHandlerInterface.setState`

--- audio_service/android/src/main/java/com/ryanheise/audioservice/AudioService.java
+++ audio_service/android/src/main/java/com/ryanheise/audioservice/AudioService.java
@@ -305,8 +305,8 @@ public class AudioService extends MediaBrowserServiceCompat {
 
     @Override
     public void onCreate() {
-        super.onCreate();
         instance = this;
+        super.onCreate();
         repeatMode = 0;
         shuffleMode = 0;
         notificationCreated = false;
